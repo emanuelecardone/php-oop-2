e06dddc2c4ce2359a433a038058e612a8a108246
emanuelecardone <e_m.cardone@yahoo.it>
2022-01-28 13:25:31 +0100

    Linkati tutti i files

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once __DIR__ . '/database.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Electronic Shop</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/frameworksizespace.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue"></script>
 </head>
 <body>
     <h1>Test</h1>
+
+    <script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
